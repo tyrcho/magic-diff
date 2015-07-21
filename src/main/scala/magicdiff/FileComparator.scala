8980fc7d73aade65dfdb1f7e103a315d8a7c5a3f
[a518291] Michel Daviot <michel.daviot@atos.net>
2015-07-21 16:06:13 +0200

    smarter to ignore comments

--- src/main/scala/magicdiff/FileComparator.scala
+++ src/main/scala/magicdiff/FileComparator.scala
@@ -6,7 +6,6 @@ import difflib.DiffUtils
 import scala.collection.JavaConversions._
 import difflib.Delta
 
-
 trait FileComparator {
   def compare(a: File, b: File): Comparison
 }
@@ -23,18 +22,29 @@ object ContentComparator extends FileComparator {
 object DiffComparator extends FileComparator {
   def readFile(f: File) = Source.fromFile(f).getLines.toList
 
+  def trimLines(lines: List[String]) = lines.filter(l => ignorePatterns.forall(p => !l.matches(p)))
+
   def compare(a: File, b: File): Comparison = {
-    val deltas = DiffUtils.diff(readFile(a), readFile(b)).getDeltas
+    val (linesA, linesB) = (readFile(a), readFile(b))
+    val deltas = DiffUtils.diff(linesA, linesB).getDeltas
     if (deltas.isEmpty) Equals
     else {
-      val diffs = deltas.filter(keep)
+      val diffs = DiffUtils.diff(trimLines(linesA), trimLines(linesB)).getDeltas
       if (diffs.isEmpty) Similar
       else Different(diffs.toList)
     }
   }
 
-  def keep(d: Delta): Boolean = {
-    val lines = (d.getOriginal.getLines ++ d.getRevised.getLines).map(_.toString.trim)
-    lines.exists(l => l.nonEmpty && !l.startsWith("import"))
-  }
+  // ignore imports, comments
+  val ignorePatterns = Seq(
+    """\s*import.*""",
+    """\s*\*.*""",
+    """\s*\/\*.*""",
+    """\s*\/\/.*""")
+
+  //  def keep(d: Delta): Boolean = {
+  //    val lines = (d.getOriginal.getLines ++ d.getRevised.getLines).map(_.toString.trim)
+  //    lines.exists(l => l.nonEmpty
+  //      && ignorePatterns.forall(p => !l.matches(p)))
+  //  }
 }
