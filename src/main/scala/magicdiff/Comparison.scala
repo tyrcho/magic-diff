850d7e897091344acec58d7d961c82dac1d578ac
[a518291] Michel Daviot <michel.daviot@atos.net>
2015-07-21 18:46:02 +0200

    smarter about formatted log messages

--- src/main/scala/magicdiff/Comparison.scala
+++ src/main/scala/magicdiff/Comparison.scala
@@ -6,12 +6,12 @@ trait Comparison { def ord: Int }
 
 case object Left extends Comparison { val ord = -4 }
 case object Right extends Comparison { val ord = -3 }
-case object Similar extends Comparison { val ord = -2 }
-case object Equals extends Comparison { val ord = -1 }
+case object Equals extends Comparison { val ord = -2 }
+case object Similar extends Comparison { val ord = -1 }
 case class Different(deltas: List[Delta]) extends Comparison {
   val ord = deltas.size
 
-  override def toString = s"${deltas.size} diffs : $deltas"
+  override def toString = s"${deltas.size} diffs : ${deltas.mkString("\n")}"
 }
 object ComparisonOrder extends Ordering[Comparison] {
   def compare(a: Comparison, b: Comparison) = b.ord - a.ord
--- src/main/scala/magicdiff/FileComparator.scala
+++ src/main/scala/magicdiff/FileComparator.scala
@@ -5,6 +5,7 @@ import scala.io.Source
 import difflib.DiffUtils
 import scala.collection.JavaConversions._
 import difflib.Delta
+import scala.annotation.tailrec
 
 trait FileComparator {
   def compare(a: File, b: File): Comparison
@@ -22,29 +23,50 @@ object ContentComparator extends FileComparator {
 object DiffComparator extends FileComparator {
   def readFile(f: File) = Source.fromFile(f).getLines.toList
 
-  def trimLines(lines: List[String]) = lines.filter(l => ignorePatterns.forall(p => !l.matches(p)))
+  def trimLines(lines: List[String]) = {
+    val uncomment = removeCommentsAndSpaces(lines)
+    val noImports = removeImports(uncomment)
+    val brackets = fixBrackets(noImports).map(_.trim)
+    val noBlanks = removeBlanks(brackets)
+    noBlanks
+  }
+
+  def removeBlanks(lines: List[String]) =
+    lines.filterNot(_.isEmpty)
+
+  def removeImports(lines: List[String]) =
+    lines.filterNot(_ matches """\s*import.*""")
+
+    
+  @tailrec
+  def fixBrackets(lines: List[String], processed: List[String] = Nil, last: String = ""): List[String] = lines match {
+    case Nil => processed
+    case h :: t =>
+      if (h.startsWith("}") && h.length > 1) {
+        val text = h.drop(1)
+        fixBrackets("}" :: t, processed ++ List(last), text)
+      } else if (h == "{") fixBrackets(t, processed, last + " {")
+      else fixBrackets(t, processed ++ List(last), h)
+  }
+
+  def removeCommentsAndSpaces(lines: List[String]) = {
+    val file = lines.map(_.trim).mkString("\n")
+    val noMulti = file.replaceAll("""(?s)\/\*.*?\*\/""", "")
+    val singleSpace = noMulti.replaceAll("""[ \t]+""", " ")
+    val noSingleComment = singleSpace.replaceAll("""\/\/.*""", " ")
+    val ignoreLogMessage = noSingleComment.replaceAll("""(?sm)format\(.*?;$""", """format("...");""")
+    ignoreLogMessage.split("\n").toList.map(_.trim)
+  }
 
   def compare(a: File, b: File): Comparison = {
     val (linesA, linesB) = (readFile(a), readFile(b))
     val deltas = DiffUtils.diff(linesA, linesB).getDeltas
     if (deltas.isEmpty) Equals
     else {
-      val diffs = DiffUtils.diff(trimLines(linesA), trimLines(linesB)).getDeltas
+      val (trimA, trimB) = (trimLines(linesA), trimLines(linesB))
+      val diffs = DiffUtils.diff(trimA, trimB).getDeltas
       if (diffs.isEmpty) Similar
       else Different(diffs.toList)
     }
   }
-
-  // ignore imports, comments
-  val ignorePatterns = Seq(
-    """\s*import.*""",
-    """\s*\*.*""",
-    """\s*\/\*.*""",
-    """\s*\/\/.*""")
-
-  //  def keep(d: Delta): Boolean = {
-  //    val lines = (d.getOriginal.getLines ++ d.getRevised.getLines).map(_.toString.trim)
-  //    lines.exists(l => l.nonEmpty
-  //      && ignorePatterns.forall(p => !l.matches(p)))
-  //  }
 }
